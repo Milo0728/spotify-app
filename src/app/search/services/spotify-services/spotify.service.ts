55410519d521ba05f2db8dbdf7da16d52fe1d4ea
Milo0728 <juamcamirodri@gmail.com>
2023-11-16 01:40:53 -0500

    ocultando environments

--- src/app/search/services/spotify-services/spotify.service.ts
+++ src/app/search/services/spotify-services/spotify.service.ts
@@ -1,19 +1,19 @@
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, map, switchMap } from 'rxjs';
-import { Artist } from '../../interfaces/artist.interface';
+import { Observable, map } from 'rxjs';
+import { environments } from 'src/environments/environments';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SpotifyService {
 
-  private apiUrl = 'https://api.spotify.com/v1';
-  private clientId = '933739476d5042248a09fae4dac224b3';
-  private clientSecret = 'cb77378250794efdb3d0c6af49c52bcb';
+  private apiUrl = environments.apiUrl;
+  private clientId = environments.clientId;
+  private clientSecret = environments.clientSecret;
 
   constructor(private _http: HttpClient) { }
-  
+
   public getToken(): Observable<string> {
     const auth = btoa(`${this.clientId}:${this.clientSecret}`);
     const headers = new HttpHeaders({
---
+++ src/environments/environments.template.ts
@@ -0,0 +1,6 @@
+export const environments = {
+    baseUrl: 'url',
+    apiUrl: 'https://api.spotify.com/v1',
+    clientId: 'client_id',
+    clientSecret: 'client_secret'
+}
